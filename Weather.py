802e0a10d70bb2af787520147a20f0e0ba687383
Kirito071008 <107003946+Kirito071008@users.noreply.github.com>
2024-03-03 23:55:21 +0100

    Update Weather.py

--- Weather.py
+++ Weather.py
@@ -1,40 +1,17 @@
 import pyfiglet,python_weather,platform
-# import the module
-
-import python_weather
-
-import asyncio
-
-import os
-
+import asyncio,os
 if platform.system() == "Windows":
-
     def clear(): os.system("cls")
-
 else:
-
     def clear(): os.system("clear")
-
 async def getweather():
-
-  # Settiamo la libreria e le unità di misura come client
-
     clear()
-
     ascii_banner = pyfiglet.figlet_format("Weather")
-
     print(ascii_banner)
-
+    print("By Kirito071008")
     async with python_weather.Client(unit=python_weather.IMPERIAL) as client:
-
-    # Prendiamo la temperatura della città
-
-      x = input("Inserisci il nome della ciità (inglese): ")
-
+      x = input("Enter the city name: ")
       weather = await client.get(f'{x}')
-
-    # Scriviamo nel terminale la temperatura
-
       z = (weather.current.temperature-32)*(5/9)
-      print(f"Temperatura attuale: {round(z)}")
+      print(f"Temperature: {round(z)}")
 asyncio.run(getweather())
